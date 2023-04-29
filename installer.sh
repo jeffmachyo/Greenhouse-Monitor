dd03c429f2b32755456f60dd0a074d0a1cec7fad
Jeff Machyo <jeffmachyo@gmail.com>
2023-04-29 14:38:48 -0400

    Updtated ssh info, still trial

--- installer.sh
+++ installer.sh
@@ -3,7 +3,19 @@
 # rm -rf out/build CMakelists.txt
 # cmake -S . -B out/build
 # make -C out/build
-ssh pi@10.42.0.84 '
+
+
+
+# sh """
+#     #!/bin/bash
+#     sudo ssh -i /path/path/keyname.pem username@serverip << EOF
+#     sudo bash /opt/filename.sh
+#     exit 0
+#     << EOF
+#     """
+
+
+ssh -i /var/lib/jenkins/.ssh/jeffs_pi pi@10.42.0.84 '
 
 ls -al
 
