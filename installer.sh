5b88d0ae66be9604671cdf8ffe9454f305048c53
Jeff Machyo <jeffmachyo@gmail.com>
2023-04-29 18:28:47 -0400

    Testing Part01 unit tests

--- installer.sh
+++ installer.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-# cd ./Version_1
-# rm -rf out/build CMakelists.txt
-# cmake -S . -B out/build
-# make -C out/build
 
 
-
-# sh """
-#     #!/bin/bash
-#     sudo ssh -i /path/path/keyname.pem username@serverip << EOF
-#     sudo bash /opt/filename.sh
-#     exit 0
-#     << EOF
-# python3 /home/pi/Documents/TELE6530/cda-lab-modules-jeffmachyo/src/test/python/programmingtheiot/part03/integration/connection/MqttClientConnectorTest.py
-# python3 /home/pi/Documents/TELE6530/cda-lab-modules-jeffmachyo/src/test/python/programmingtheiot/part01/unit/common/ConfigUtilTest.py
-#     """
+# List of steps that will be carried out by the Jenkins server on the target Raspberry Pi. The idea is 
+# to conduct all the tests from the /tmp folder and then once the tests are complete, create a Docker image
+# that can be shipped and deploy it on the Raspberry Pi.
 scp -i /var/lib/jenkins/.ssh/jeffs_pi -r /var/lib/jenkins/workspace/Greenhouse-Monitor pi@10.42.0.84:/tmp
 
 ssh -i /var/lib/jenkins/.ssh/jeffs_pi pi@10.42.0.84 '
@@ -22,8 +11,8 @@ ssh -i /var/lib/jenkins/.ssh/jeffs_pi pi@10.42.0.84 '
 PYTHONPATH="/tmp/Greenhouse-Monitor/src/main/python:/tmp/Greenhouse-Monitor/src/test/python:$PYTHONPATH"
 export PYTHONPATH
 python3 /tmp/Greenhouse-Monitor/src/test/python/programmingtheiot/part01/unit/system/SystemCpuUtilTaskTest.py
-
-
+python3 /tmp/Greenhouse-Monitor/src/test/python/programmingtheiot/part01/unit/system/SystemMemUtilTaskTest.py
+python3 /tmp/Greenhouse-Monitor/src/test/python/programmingtheiot/part01/integration/system/SystemPerformanceManagerTest.py
 
 '
 
