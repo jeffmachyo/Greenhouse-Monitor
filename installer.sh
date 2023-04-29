1b6b8b2365ae6bb084c9a564fb316616d9ca1bed
Jeff Machyo <jeffmachyo@gmail.com>
2023-04-29 18:02:40 -0400

    Testing temp pythonpath

--- installer.sh
+++ installer.sh
@@ -13,17 +13,17 @@
 #     exit 0
 #     << EOF
 # python3 /home/pi/Documents/TELE6530/cda-lab-modules-jeffmachyo/src/test/python/programmingtheiot/part03/integration/connection/MqttClientConnectorTest.py
+# python3 /home/pi/Documents/TELE6530/cda-lab-modules-jeffmachyo/src/test/python/programmingtheiot/part01/unit/common/ConfigUtilTest.py
 #     """
 scp -i /var/lib/jenkins/.ssh/jeffs_pi -r /var/lib/jenkins/workspace/Greenhouse-Monitor pi@10.42.0.84:/tmp
 
 ssh -i /var/lib/jenkins/.ssh/jeffs_pi pi@10.42.0.84 '
-df -h
 
-PYTHONPATH="/home/pi/Documents/TELE6530/cda-lab-modules-jeffmachyo/src/main/python:/home/pi/Documents/TELE6530/cda-lab-modules-jeffmachyo/src/test/python:$PYTHONPATH"
+PYTHONPATH="/tmp/Greenhouse-Monitor/src/main/python:/tmp/Greenhouse-Monitor/src/test/python:$PYTHONPATH"
 export PYTHONPATH
-python3 /home/pi/Documents/TELE6530/cda-lab-modules-jeffmachyo/src/test/python/programmingtheiot/part01/unit/system/SystemCpuUtilTaskTest.py
+python3 /tmp/Greenhouse-Monitor/src/test/python/programmingtheiot/part01/unit/system/SystemCpuUtilTaskTest.py
 
-python3 /home/pi/Documents/TELE6530/cda-lab-modules-jeffmachyo/src/test/python/programmingtheiot/part01/unit/common/ConfigUtilTest.py
+
 
 '
 
